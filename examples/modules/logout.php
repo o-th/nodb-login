0bbc37391eaafc4ae8862e2d189e907ff15be35d
Isaac Steinbrook <5461241+isteinbrook@users.noreply.github.com>
2018-11-03 16:20:30 -0700

    Update logout.php

--- examples/modules/logout.php
+++ examples/modules/logout.php
@@ -1,7 +1,10 @@
+<!-- Initialize logout function -->
 <?php 
 if(isset($_GET['logout'])) {
     $_SESSION['username'] = '';
     header('Location:  ' . $_SERVER['PHP_SELF']);
 }
 ?>
+
+<!-- anchor tag to activate logout function -->
 <p style="text-align:right;">You are logged in as <?=$_SESSION['username']?> <a href="?logout=1">Logout</a></p>
