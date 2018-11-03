0aba26ca44b63bdb0478b4834000d2a3b904e347
Isaac Steinbrook <5461241+isteinbrook@users.noreply.github.com>
2018-11-03 15:54:59 -0700

    Delete logout.php

--- example/logout.php
+++
@@ -1,7 +0,0 @@
-<?php 
-if(isset($_GET['logout'])) {
-    $_SESSION['username'] = '';
-    header('Location:  ' . $_SERVER['PHP_SELF']);
-}
-?>
-<p style="text-align:right;">You are logged in as <?=$_SESSION['username']?> <a href="?logout=1">Logout</a></p>
